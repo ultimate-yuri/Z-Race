24bbfc35ad1975c7e509cbf754beb87eb292405f
ultimate-yuri <ultimate_yuri@outlook.com>
2026-07-13 15:50:08 +0300

    Delete gamemodes/zcity/gamemode/modes/scugarena directory

--- gamemodes/zcity/gamemode/modes/scugarena/sh_arena.lua
+++
@@ -1,8 +0,0 @@
-local MODE = MODE
-
-hook.Add("StartCommand", "DisallowShosting", function(ply, mv)
-	if zb.CROUND == "scugarena" and (zb.ROUND_START or 0) + 20 > CurTime() then
-		mv:RemoveKey(IN_ATTACK)
-		mv:RemoveKey(IN_ATTACK2)
-	end
-end)
